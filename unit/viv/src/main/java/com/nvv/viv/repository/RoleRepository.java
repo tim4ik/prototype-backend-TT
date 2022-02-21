edc19916a7f037d7117985f63b51f04a597cb1f7
vadym narchuk <uniondirfolder@gmail.com>
2022-02-21 12:12:10 +0200

    Add ROLES

--- unit/viv/src/main/java/com/nvv/viv/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nvv.viv.repository;
-
-import com.nvv.viv.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
